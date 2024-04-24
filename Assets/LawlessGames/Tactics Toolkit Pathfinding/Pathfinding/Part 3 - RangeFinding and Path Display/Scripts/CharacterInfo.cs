0a4f8ff29a39808513b31923cb33add71ab5a46d
Benjamin <benjamin.arctander@gmail.com>
2024-04-24 22:20:59 +0200

    Organising Project folders and removing old

--- Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/CharacterInfo.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace LawlessGames.Tactics_Toolkit_Pathfinding.Pathfinding.Part_3___RangeFinding_and_Path_Display.Scripts
-{
-    public class CharacterInfo : MonoBehaviour
-    {
-        public OverlayTile standingOnTile;
-    }
-}
